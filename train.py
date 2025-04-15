072bd58fd17605c497113dfc77591f73905e2683
DOL-DOM <haesolj@naver.com>
2025-04-15 12:17:40 +0900

    initial setting for MNIST

---
+++ train.py
@@ -0,0 +1,11 @@
+#라이브러리
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from torchvision import transforms, datasets
+
