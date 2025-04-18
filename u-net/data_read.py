bd5249d3a23814ff04eb80bbfe4d48ffa7a32607
DOL-DOM <haesolj@naver.com>
2025-04-19 03:19:24 +0900

    Add folder: u-net

---
+++ u-net/data_read.py
@@ -0,0 +1,17 @@
+# %%
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+# %%
+dir_data = './data'
+
+name_label = 'train-labels.tif'
+name_input = 'train-volume.tif'
+
+img_label = Image.open(os.path.join(dir_data, name_label))
+img_input = Image.open(os.path.join(dir_data, name_input))
+
+ny, nx = img_label.size
+nframe = img_label.n_frames
+# %%
