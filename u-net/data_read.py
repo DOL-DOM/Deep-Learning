7b7b9b281fa1dc0d394037ff078e7c5b21933310
DOL-DOM <haesolj@naver.com>
2025-04-21 12:07:50 +0900

    Add U-Net

--- u-net/data_read.py
+++ u-net/data_read.py
@@ -11,7 +11,85 @@ name_input = 'train-volume.tif'
 
 img_label = Image.open(os.path.join(dir_data, name_label))
 img_input = Image.open(os.path.join(dir_data, name_input))
-
+# ny, nx 한 프레임의 이미지 크기 (세로, 가로)
 ny, nx = img_label.size
+# 전체 프레임 수 (이미지가 여러 장 들어 있는 tif 파일)
 nframe = img_label.n_frames
+
 # %%
+#train, validation, test: propotion & directory
+nframe_train = 24
+nframe_val = 3
+nframe_test = 3
+
+dir_save_train = os.path.join(dir_data, 'train')
+dir_save_val = os.path.join(dir_data, 'val')
+dir_save_test = os.path.join(dir_data, 'test')
+
+if not os.path.exists(dir_save_train):
+    os.makedirs(dir_save_train)
+
+if not os.path.exists(dir_save_val):
+    os.makedirs(dir_save_val)
+
+if not os.path.exists(dir_save_test):
+    os.makedirs(dir_save_test)
+
+# %%
+id_frame = np.arange(nframe)      # [0, 1, 2, ..., 29] → 프레임 번호 배열 생성
+np.random.shuffle(id_frame)       # 무작위로 섞음! (여기서 랜덤 기준이 됨)
+
+
+# %%
+#1. training set (24개)
+offset_nframe = 0
+
+for i in range(nframe_train):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_train, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_train, 'input_%03d.npy' % i), input_)
+
+# %%
+#2. validation set 
+offset_nframe = nframe_train 
+
+for i in range(nframe_val):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_val, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_val, 'input_%03d.npy' % i), input_)
+
+#%%
+#3.test set
+offset_nframe = nframe_train + nframe_val
+
+for i in range(nframe_test):
+    img_label.seek(id_frame[i + offset_nframe])
+    img_input.seek(id_frame[i + offset_nframe])
+
+    label_ = np.asarray(img_label)
+    input_ = np.asarray(img_input)
+
+    np.save(os.path.join(dir_save_test, 'label_%03d.npy' % i), label_)
+    np.save(os.path.join(dir_save_test, 'input_%03d.npy' % i), input_)
+
+#%%
+#segmentation map
+plt.subplot(121)
+plt.imshow(label_, cmap='gray')
+plt.title('label')
+
+plt.subplot(122)
+plt.imshow(input_, cmap='gray')
+plt.title('input')
+
+plt.show()
